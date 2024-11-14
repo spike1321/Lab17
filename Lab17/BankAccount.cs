bf035c0e14b1ac272079a1678ce7de6f907b244e
Павел Каплин <spike1321@yandex.ru>
2024-11-14 22:45:37 +0300

    Добавьте файлы проекта.

---
+++ Lab17/BankAccount.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab17
+{
+    class BankAccount<T>
+    {
+        private T Count_Number;
+        private double Balance;
+        private string Full_Name;
+
+        public BankAccount(T count_number, double balance, string fulname)
+        {
+            this.Count_Number = count_number;
+            this.Balance = balance;
+            this.Full_Name = fulname;
+        }
+        public T GetCount_Number()
+        {
+            return Count_Number;
+        }
+
+        public double GetBalance()
+        {
+            return Balance;
+        }
+
+        public string GetFull_Name()
+        {
+            return Full_Name;
+        }
+        public void SetCount_Number(T count_number)
+        {
+            this.Count_Number = count_number;
+        }
+        public void SetBalance(double balance)
+        {
+            this.Balance = balance;
+        }
+        public void SetFull_Name(string fulname)
+        {
+            this.Full_Name = fulname;
+        }
+        
+    }
+}
---
+++ Lab17/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите номер счёта числом: ");
+                int count_number = int.Parse(Console.ReadLine());
+                Console.Write("Введите ФИО: ");
+                string full_name = Console.ReadLine();
+                Console.Write("Введите баланс: ");
+                double balance = double.Parse(Console.ReadLine());
+                BankAccount<int> bankAccount1 = new BankAccount<int>(count_number, balance, full_name);
+                Console.Write("Введите номер счёта строкой: ");
+                string count_number2 = Console.ReadLine();
+                Console.Write("Введите ФИО: ");
+                string full_name2 = Console.ReadLine();
+                Console.Write("Введите баланс: ");
+                double balance2 = double.Parse(Console.ReadLine());
+
+                BankAccount<string> bankAccount2 = new BankAccount<string>(count_number2, balance2, full_name2);
+                Console.WriteLine("Номер счёта: {0} ФИО: {1} Баланс: {2}", bankAccount1.GetCount_Number(), bankAccount1.GetFull_Name(), bankAccount1.GetBalance());
+                Console.WriteLine("Номер счёта: {0} ФИО: {1} Баланс: {2}", bankAccount2.GetCount_Number(), bankAccount2.GetFull_Name(), bankAccount2.GetBalance());
+
+                bankAccount1.SetCount_Number(6584999);
+                bankAccount1.SetBalance(1500.00);
+                bankAccount1.SetFull_Name("Блинов Даниил Максимович");
+
+                bankAccount2.SetCount_Number("CD53421213");
+                bankAccount2.SetBalance(2500.00);
+                bankAccount2.SetFull_Name("Симонова Агата Денисовна");
+
+                Console.WriteLine("Номер счёта: {0} ФИО: {1} Баланс: {2}", bankAccount1.GetCount_Number(), bankAccount1.GetFull_Name(), bankAccount1.GetBalance());
+                Console.WriteLine("Номер счёта: {0} ФИО: {1} Баланс: {2}", bankAccount2.GetCount_Number(), bankAccount2.GetFull_Name(), bankAccount2.GetBalance());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
